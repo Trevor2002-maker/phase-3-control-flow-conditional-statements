01df4022a7df68970824254a85e5ca807f7a2ee1
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-16 10:22:50 +0300

    commit tree 73d3f7fcea190bb6b6ce3e256d42045c31dd2333

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,57 @@
 def admin_login(username, password)
-  # your code here
+    if username.upcase == "ADMIN" && password == "12345"
+      return "Access granted"
+    else
+      return "Access denied"
+    end
 end
 
+puts admin_login("admin", "12345")
+puts admin_login("trevor", "12345")
+puts admin_login("ADMIN", "12345")
+
 def hows_the_weather(temperature)
-  # your code here
+    if temperature < 40
+      return "It's brisk out there!"
+    elsif temperature >= 40 && temperature <= 65
+    return "It's a little chilly out there!"
+    elsif temperature > 85
+    return "It's too dang hot out there!"
+    else 
+      return "It's perfect out there!"
+    end
 end
 
 def fizzbuzz(num)
-  # your code here
+    if num % 3 == 0 && num % 5 == 0
+      return "FizzBuzz"
+    elsif num % 3 == 0 
+      return "Fizz"
+    elsif num % 5 == 0
+      return "Buzz"
+    else
+      return num
+    end
 end
 
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
+puts fizzbuzz(7)
+
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    return num1 + num2
+  when "-"
+    return num1 - num2
+  when "*"
+    return num1 * num2
+  when "/"
+    return num1 / num2
+  else
+    puts "Invalid operation!"
+    return nil
+  end
 end
 
